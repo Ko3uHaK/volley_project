0438d39e248560c646c9003e02e75ac5e0b42386
daniil.kachura <daniil.kachura@innowise-group.com>
2023-08-04 14:04:46 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,128 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  // Функция для загрузки данных о командах и игроках
+  async function loadTeamsAndPlayers() {
+    const response = await fetch('/teams');
+
+    if (response.ok) {
+      const teams = await response.json();
+      displayTeams(teams);
+      populateTeamSelects(teams);
+    } else {
+      console.error('Error fetching teams data');
+    }
+  }
+  
+  function displayTeams(teams) {
+    const teamList = document.getElementById('teamList');
+    teamList.innerHTML = ''; // Очищаем содержимое списка
+  
+    teams.forEach(team => {
+      const listItem = document.createElement('li');
+      listItem.textContent = team.name;
+      teamList.appendChild(listItem);
+    });
+  }
+  
+  function displayPlayers(players) {
+    const roster1 = document.getElementById('roster1');
+    const roster2 = document.getElementById('roster2');
+    roster1.innerHTML = ''; // Очищаем содержимое списка
+    roster2.innerHTML = ''; // Очищаем содержимое списка
+  
+    players.forEach(player => {
+      const listItem = document.createElement('li');
+      listItem.textContent = player.name;
+  
+      if (player.team_id === parseInt(teamSelect1.value)) {
+        roster1.appendChild(listItem);
+      } else if (player.team_id === parseInt(teamSelect2.value)) {
+        roster2.appendChild(listItem);
+      }
+    });
+  }
+  
+  // Функция для заполнения выпадающих списков командами
+  function populateTeamSelects(teams) {
+    const teamSelect1 = document.getElementById('teamSelect1');
+    const teamSelect2 = document.getElementById('teamSelect2');
+
+    teams.forEach(team => {
+      const option = document.createElement('option');
+      option.value = team.id; // Предполагается, что у команды есть уникальный идентификатор (id) в базе данных
+      option.textContent = team.name;
+
+      // Добавляем опции в оба выпадающих списка
+      teamSelect1.appendChild(option.cloneNode(true));
+      teamSelect2.appendChild(option);
+    });
+
+    // Добавляем обработчики событий для изменения выбранных команд
+    teamSelect1.addEventListener('change', handleTeamSelectChange);
+    teamSelect2.addEventListener('change', handleTeamSelectChange);
+  }
+
+  // Обработчик события изменения выбранной команды
+  async function handleTeamSelectChange(event) {
+    // Получаем идентификатор выбранной команды
+    const teamId = event.target.value;
+  
+    // Загружаем данные об игроках выбранной команды с сервера
+    const response = await fetch(`/team/${teamId}/players`);
+  
+    if (response.ok) {
+      const players = await response.json();
+      if (event.target.id === 'teamSelect1') {
+        displayPlayers(players, 'roster1'); // Отображаем данные об игроках на странице для команды 1
+      } else if (event.target.id === 'teamSelect2') {
+        displayPlayers(players, 'roster2'); // Отображаем данные об игроках на странице для команды 2
+      }
+    } else {
+      console.error('Error fetching players data');
+    }
+  }
+  
+  function displayPlayers(players, listId) {
+    const rosterList = document.getElementById(listId);
+    rosterList.innerHTML = ''; // Очищаем содержимое списка
+  
+    players.forEach(player => {
+      const listItem = document.createElement('li');
+      listItem.textContent = player.name;
+      rosterList.appendChild(listItem);
+    });
+  }
+  
+  // Обработчик события отправки формы
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const time = formData.get('time');
+    const date = formData.get('date');
+    const venue = formData.get('venue');
+    const team1Id = formData.get('teamSelect1');
+    const team2Id = formData.get('teamSelect2');
+
+    // Отправляем данные на сервер (можно использовать fetch)
+    const response = await fetch('/save-game', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ time, date, venue, team1Id, team2Id }),
+    });
+
+    if (response.ok) {
+      // Если данные успешно отправлены на сервер, загружаем информацию о командах и игроках
+      await loadTeamsAndPlayers();
+    } else {
+      console.error('Error saving game data');
+    }
+  }
+
+  // Добавляем обработчик события отправки формы
+  const form = document.getElementById('gameForm');
+  form.addEventListener('submit', handleFormSubmit);
+
+  // Загружаем данные о командах и игроках при загрузке страницы
+  await loadTeamsAndPlayers();
+});
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+const cors = require('cors');
+const path = require('path');
+const pool = new Pool({
+  user: 'user1',
+  host: 'localhost',
+  database: 'volleyball_db',
+  password: '252525',
+  port: 5432,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+// Обработчик для запросов на получение списка команд
+app.get('/teams', async (req, res) => {
+  try {
+    const teamsQuery = 'SELECT * FROM teams';
+    const teamsResult = await pool.query(teamsQuery);
+    const teams = teamsResult.rows;
+    res.json(teams);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Обработчик для запросов на получение списка игроков для команды
+app.get('/team/:teamId/players', async (req, res) => {
+  const teamId = req.params.teamId;
+  try {
+    const playersQuery = 'SELECT * FROM players WHERE team_id = $1';
+    const playersResult = await pool.query(playersQuery, [teamId]);
+    const players = playersResult.rows;
+    res.json(players);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Обработчик для запросов на сохранение данных игры
+app.post('/save-game', async (req, res) => {
+  const { time, date, venue, team1Id, team2Id } = req.body;
+  try {
+    // Здесь вы можете сохранить данные игры в базу данных
+    // Или выполнить другую логику, связанную с сохранением игры
+    // Например, можно создать отдельную таблицу для хранения данных игр
+    // Или использовать существующие таблицы для сохранения данных игр
+
+    // В этом примере просто возвращаем успешный ответ
+    res.status(200).json({ message: 'Game data saved successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Сервер запущен на порту http://localhost:3000');
+});
